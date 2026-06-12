afb75afb03fdb2e7641282c809c8d9582c5c5541
vishalanandsadh13 <vishalanandsadh@gmail.com>
2026-06-12 23:34:17 +0530

    reverse manual

---
+++ Questions/Array/Array-reverse/arrayreverse.js
@@ -0,0 +1,12 @@
+function reverseArray(arr) {
+    let reversedArr = []; 
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversedArr.push(arr[i]); 
+    } 
+    return reversedArr;
+}
+
+// Example usage:
+const originalArray = [1, 2, 3, 4, 5];
+const reversedArray = reverseArray(originalArray);
+console.log(reversedArray); // Output: [5, 4, 3, 2, 1]  
