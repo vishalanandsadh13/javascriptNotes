3d47159906ddc8bc656a0f17d84a36c3254b614b
vishalanandsadh13 <vishalanandsadh@gmail.com>
2026-06-14 18:06:08 +0530

    .

--- Questions/Array/Array-reverse/arrayreverse.js
+++ Questions/Array/Array-reverse/arrayreverse.js
@@ -9,4 +9,4 @@ function reverseArray(arr) {
 // Example usage:
 const originalArray = [1, 2, 3, 4, 5];
 const reversedArray = reverseArray(originalArray);
-console.log(reversedArray); // Output: [5, 4, 3, 2, 1]  
+console.log(reversedArray); 
